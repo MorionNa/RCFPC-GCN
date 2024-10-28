88ace6ad800a6620a2677d22d93a54f4cb596353
Senna Wang <67867064+MorionNa@users.noreply.github.com>
2024-10-28 22:10:58 +0800

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -4,6 +4,6 @@ REM 激活虚拟环境
 call %~dp0venv\scripts\activate.bat
 REM 执行 Python 脚本
 python prediction.py
-set "BLENDER_PATH=%~dp0blender-2.92\blender.exe"
+set "BLENDER_PATH=%~dp0blender-2.92.0-windows64\blender.exe"
 set "BLEND_FILE=%~dp0Visualization.blend"
 "%BLENDER_PATH%" "%BLEND_FILE%"
